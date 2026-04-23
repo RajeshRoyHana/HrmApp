d30bd7a24b729f4306a6d47d6326e71b14245548
RajeshRoyHana <rajesh.roy@hana-ltd.com>
2026-04-23 18:09:09 +0600

    added: employee model added

--- HrmClientApp/src/app/app.ts
+++ HrmClientApp/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('HrmClientApp');
+  protected readonly title = signal('Hrm Application');
 }
---
+++ HrmClientApp/src/app/features/Employee/emplyee-list/emplyee-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emplyee-list',
+  imports: [],
+  templateUrl: './emplyee-list.html',
+  styleUrl: './emplyee-list.css',
+})
+export class EmplyeeList {
+
+}
